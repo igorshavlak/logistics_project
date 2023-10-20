dae97fb143140bf598c7528adf13ad2f5900d088
igorshavlak <igorshavlak8@gmail.com>
2023-10-20 14:49:43 +0300

    commit project

---
+++ src/main/java/com/logistics/project/ContactController.java
@@ -0,0 +1,30 @@
+package com.logistics.project;
+
+import com.logistics.project.ContactForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ContactController {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @PostMapping("/sendEmail")
+    public ResponseEntity<String> sendEmail(@RequestBody ContactForm contactForm) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("your-email@example.com");
+        message.setTo("recipient@example.com");
+        message.setSubject("New Contact Request");
+        message.setText("Name: " + contactForm.getName() + "\nPhone Number: " + contactForm.getPhoneNumber());
+        emailSender.send(message);
+        return ResponseEntity.ok("Email sent successfully");
+    }
+}
---
+++ src/main/java/com/logistics/project/ContactForm.java
@@ -0,0 +1,22 @@
+package com.logistics.project;
+
+public class ContactForm {
+    private String name;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+}
+
